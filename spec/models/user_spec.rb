2bbc6b64b3671d762438a4150fa8e4dba3b04ba0
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-06 00:24:51 +0100

    Create association and validation tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should have_many(:restaurants).dependent(:destroy) }
+
+  it { should have_many :collections }
+  it { should have_many :friends }
+  
+  # Validation tests
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 end
