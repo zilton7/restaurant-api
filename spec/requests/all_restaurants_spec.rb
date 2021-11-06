8ee0b914cdc58ebe7eabde6dd7768a891744539b
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-06 19:35:20 +0100

    Create AllRestaurants controller request rspec test

--- spec/requests/all_restaurants_spec.rb
+++ spec/requests/all_restaurants_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'AllRestaurants', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:example) do
+    @all_restaurant = AllRestaurant.create(name: 'Kushi Tsuru', opening_date: 'Mon-Sun 11:30 am - 9 pm')
+  end
+
+  it 'shows all all_restaurants' do
+    get '/api/v1/all_restaurants'
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'displays all_restaurant information' do
+    get "/api/v1/all_restaurants/#{@all_restaurant.id}"
+    expect(response).to have_http_status(:ok)
   end
 end
