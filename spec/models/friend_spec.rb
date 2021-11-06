d5e66948a9bb9caea97db3855401a0b8396ec394
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-06 10:21:18 +0100

    Create validation and association test for Friend model

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Friend, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:user) }
+
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
 end
